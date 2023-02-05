9736ab3919f9050736840dc18d4f4c0a32e13f45
Ivan <ivan.compagnucci@studenti.unicam.it>
2023-02-05 16:13:00 +0100

    Project Initialization: Spring Boot and React.js

---
+++ src/main/backend/com/pros/bpmninspector/BpmnInspectorApplication.java
@@ -0,0 +1,12 @@
+package com.pros.bpmninspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnInspectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BpmnInspectorApplication.class, args);
+    }
+
+}
