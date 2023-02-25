44541d6a0559826f9ccd054389063cb308570eba
Ivan <ivan.compagnucci@studenti.unicam.it>
2023-02-25 17:53:16 +0100

    Added graphical improvements and post upload news

--- frontend/views/helloreact/HelloReactView.tsx
+++ frontend/views/helloreact/HelloReactView.tsx
@@ -3,6 +3,7 @@ import { TextField } from '@hilla/react-components/TextField.js';
 import {Checkbox} from "@hilla/react-components/Checkbox.js";
 import {VAADIN_CSRF_HEADER} from "@hilla/frontend/CsrfUtils";
 import React, {ReactDOM, useState} from "react";
+import "./uploadtemplate.css";
 import { Button } from '@hilla/react-components/Button.js';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -29,7 +30,20 @@ export default function HelloReactView() {
             return (
                 <>
                     <div className="flex flex-col h-full items-left justify-left p-l text-left box-border">
-                        <h1>BPMN Models Inspected!</h1>
+                        <a style={{fontSize:'40px',color:'black',alignSelf:'left',fontWeight:"bold"}}>List of BPMN Models Uploaded</a>
+
+                        <section className="p-m gap-m items-end">
+                            <h3>Filtering options:</h3>
+                            <Checkbox value='0' label='Remove Duplicate models'/>
+                            <br/>
+                            <Checkbox value='1' label='Remove Invalid models'/>
+                            <br/>
+                            <Checkbox value='2' label='Remove non-English models' disabled checked/>
+                            <br/>
+
+                            <input style={{background:'#10ad73', color: 'white', fontSize: '20px', padding: '10px 40px', borderRadius: '5px', border: 'none', cursor: 'pointer', marginTop: '0.42cm'}} type="submit" value="Inspect"/>
+
+                        </section>
                     </div>
                 </>
             );
@@ -51,8 +65,6 @@ export default function HelloReactView() {
                     theme: "colored",
                 });
             }
-
-
 
             // @ts-ignore
             if(getExtension(selectedFile[0].name).toLowerCase() !== "zip" && getExtension(selectedFile[0].name).toLowerCase() !== "xml" && getExtension(selectedFile[0].name).toLowerCase() !== "bpmn") {
@@ -109,10 +121,11 @@ export default function HelloReactView() {
         }
 
         return (
-            <div id={"container"}>
-                <h1>
-                    <td>BPMN Inspector</td>
-                </h1>
+            <div className="flex flex-col h-full items-left justify-left p-l text-left box-border">
+
+                <a style={{fontSize:'40px',color:'black',alignSelf:'center',fontWeight:"bold"}}>BPMN Models Inspector</a>
+                <a style={{fontSize:'20px',color:'black',alignSelf:'center'}}>Upload one or more .bpmn, .xml or .zip files.</a>
+                <div className="form-container">
                 <section className="flex flex-col h-full items-center p-l text-center box-border" style={{
                     cursor: "pointer",
                     borderStyle: "solid",
@@ -124,30 +137,32 @@ export default function HelloReactView() {
                     justifyContent: "center",
                     alignItems: "center",
                     height: "300px",
-                    border: "3px dashed rgb(0,0,0)",
+                    border: "4px dashed #10ad73",
                     padding: "10em",
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
                 }}>
 
-                        <h4>
-                            <td>Import BPMN models</td>
-                        </h4>
+
+                    <div className="image-container">
+                        <svg className="float-svg-end"></svg>
+                        <svg className="float-svg-xor"></svg>
+                        <svg className="float-svg-interMsg"></svg>
+                    </div>
+
+
                     <form encType={"multipart/form-data"} onSubmit={onClick}>
-                        <input type="file" name="file" id="file" onChange={handleFileSelect} multiple/>
-                        <input type="submit" value="Inspect!" style={{color: "green", cursor: "pointer"}}/>
+                        <input style={{background:'#10ad73', color: 'white', fontSize: '20px', padding: '10px 40px', borderRadius: '5px', border: 'none', cursor: 'pointer', marginTop: '0.42cm'}}
+                               type="file" name="file" id="file" onChange={handleFileSelect} multiple/>
+                        <input style={{background:'#10ad73', color: 'white', fontSize: '20px', padding: '10px 40px', borderRadius: '5px', border: 'none', cursor: 'pointer', marginTop: '0.42cm'}}
+                               type="submit" value="Upload models"/>
                     </form>
                 </section>
-
-                <section className="p-m gap-m items-end">
-                    <h3>Filtering options:</h3>
-                        <Checkbox value='0' label='Remove Duplicate models'/>
-                        <br/>
-                        <Checkbox value='1' label='Remove Invalid models'/>
-                        <br/>
-                        <Checkbox value='2' label='Remove non-English models' disabled checked
-                        />
-                        <br/>
-                </section>
             </div>
+                <a style={{fontSize:'15px',color:'black',fontStyle:"italic",marginTop:'1cm'}}>Note: All files not in .bpmn or .xml format will be deleted after the uploading</a>
+
+            </div>
+
         )
     }
     //
@@ -155,7 +170,7 @@ export default function HelloReactView() {
     return (
         <>
             { showHome ? <Home /> : null }
-                { showResults ? <Results /> : null }
+            { showResults ? <Results /> : null }
         </>
     );
 }
