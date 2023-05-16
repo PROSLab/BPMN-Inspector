5c5460febd5b6868854f232afb522d70926c58c7
Ivan <ivan.compagnucci@studenti.unicam.it>
2023-05-16 19:02:24 +0200

    Huge improvements on the execution time of the python script: Deleted absents elements

--- src/main/resources/bpmnCounterOutput/pearson.py
+++ src/main/resources/bpmnCounterOutput/pearson.py
@@ -7,26 +7,30 @@ import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 csv_file = os.path.join(current_dir, 'bpmn_elements.csv')
-df = pd.read_csv(csv_file, low_memory=False, delimiter=";", encoding = "ISO-8859-1", index_col=False)
+df = pd.read_csv(csv_file, low_memory=False, delimiter=";", encoding="ISO-8859-1", index_col=False)
 df = df.iloc[:, 2:-7]
 np.seterr(divide='ignore', invalid='ignore')
 filename = os.path.join(current_dir, "bpmn_combined.csv")
 header = df.columns.values.tolist()
-header.insert(0, '\\' )
+header.insert(0, '\\')
 
 if len(df) <= 1:
     print("Only one model is present")
     exit()
 
+# Trova gli elementi che sono presenti almeno una volta nel dataset
+present_elements = df.columns[df.any()].tolist()
+
 with open(filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(header)
-for index, element in enumerate(df):
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(header)
+
+for index, element in enumerate(present_elements):
     rho_element_list = []
     x_df = np.array(df.loc[0:, element])
-    for index2, element2 in enumerate(df):
+    for index2, element2 in enumerate(present_elements):
         y_df = np.array(df.loc[0:, element2])
-        my_rho = np.corrcoef(x_df,y_df)
+        my_rho = np.corrcoef(x_df, y_df)
         currentRho = my_rho[1][0]
         rho_element_list.append(currentRho)
     with open(filename, 'a', newline='') as csvfile:
