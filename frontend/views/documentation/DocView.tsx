894c82668d9551dceaf503bc428a6bc58bb35a34
Ivan <ivan.compagnucci@studenti.unicam.it>
2023-06-05 12:23:08 +0200

    Added images for the README file

--- frontend/views/documentation/DocView.tsx
+++ frontend/views/documentation/DocView.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import MarkdownComponent from "Frontend/components/markdownreader/MarkdownComponent";
+import axios from "axios";
 
 export default function DocView() {
-    const markdownContent = '# Titolo del documento\n\nQuesto è un paragrafo di esempio.';
+    const [markdownContent, setMarkdownContent] = useState("");
+
+    useEffect(() => {
+        // Effettua la richiesta HTTP per ottenere il contenuto del file README.md
+        axios
+            .get("https://raw.githubusercontent.com/PROSLab/BPMN-Inspector/master/README.md")
+            .then((response) => {
+                setMarkdownContent(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     return (
         <div>
