63556b1dc968d89e4a8b93d61f0179d4b98858d0
Ivan <ivan.compagnucci@studenti.unicam.it>
2023-06-19 12:15:31 +0200

    Bug fixes

--- src/main/java/com/example/application/BEBoP/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/labeling/LabelingActivities.java
+++ src/main/java/com/example/application/BEBoP/src/main/java/eu/learnpad/verification/plugin/bpmn/guideline/impl/labeling/LabelingActivities.java
@@ -1,8 +1,6 @@
 package com.example.application.BEBoP.src.main.java.eu.learnpad.verification.plugin.bpmn.guideline.impl.labeling;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 import org.eclipse.bpmn2.Activity;
 import org.eclipse.bpmn2.CallActivity;
@@ -11,11 +9,9 @@ import org.eclipse.bpmn2.FlowElement;
 import org.eclipse.bpmn2.Process;
 import org.eclipse.bpmn2.RootElement;
 import org.eclipse.bpmn2.SubProcess;
-import org.eclipse.bpmn2.di.BPMNShape;
 
 import com.example.application.BEBoP.src.main.java.eu.learnpad.verification.plugin.bpmn.guideline.Messages;
 import com.example.application.BEBoP.src.main.java.eu.learnpad.verification.plugin.bpmn.guideline.impl.abstractGuideline;
-import com.example.application.BEBoP.src.main.java.eu.learnpad.verification.plugin.bpmn.reader.BPMNUtils;
 
 public class LabelingActivities extends abstractGuideline {
 
@@ -118,76 +114,57 @@ public class LabelingActivities extends abstractGuideline {
 		}
 	}
 
-	protected void searchSubProcess(SubProcess sub) {
-		StringBuilder temp = new StringBuilder();
-		List<FlowElement> activities = new ArrayList<FlowElement>();
-		int num = 0;
-		boolean flag = true;
-		
-		for (FlowElement fe : sub.getFlowElements()) {
-			if (fe instanceof SubProcess) {
-				SubProcess ssub = (SubProcess) fe;
-				if (ssub.getName() == null || ssub.getName().length() < 4) {
-					num++;
-					elementsBPMN.add(fe);
-					String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l); //$NON-NLS-1$
-					setElements(fe.getId(), IDProcess, name);
-					temp.append("* name=" + name + " ID=" + fe.getId() //$NON-NLS-1$ //$NON-NLS-2$
-							+ "\n"); //$NON-NLS-1$
-					this.searchSubProcess(sub);
-				} else {
-					activities.add(ssub);
-					this.searchSubProcess(ssub);
-				}
+	Set<FlowElement> visitedElements = new HashSet<>();
+	List<FlowElement> elementsBPMN = new ArrayList<>();
+	List<Activity> activities = new ArrayList<>();
+	StringBuilder temp = new StringBuilder();
+	int num = 0;
 
-			} else if (fe instanceof CallActivity) {
-				CallActivity ca = (CallActivity) fe;
-				if (ca.getName() == null || ca.getName().length() < 4) {
-					num++;
-					elementsBPMN.add(fe);
-					String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l); //$NON-NLS-1$
-					setElements(fe.getId(), IDProcess, name);
-					temp.append("* name=" + name + " ID=" + fe.getId() //$NON-NLS-1$ //$NON-NLS-2$
-							+ "\n"); //$NON-NLS-1$
-				}
-			} else if (fe instanceof Activity) {
-				Activity a = (Activity) fe;
-				if (a.getName() == null || a.getName().length() < 4) {
-					num++;
-					elementsBPMN.add(fe);
-					String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l); //$NON-NLS-1$
-					setElements(fe.getId(), IDProcess, name);
-					temp.append("* name=" + name + " ID=" + fe.getId() //$NON-NLS-1$ //$NON-NLS-2$
-							+ "\n"); //$NON-NLS-1$
-
-				} else {
-					activities.add(a);
-				}
-			}
+	public void searchSubProcess(FlowElement fe) {
+		if (visitedElements.contains(fe)) {
+			return;
 		}
 
-		FlowElement a1 = null;
-		for (FlowElement a2 : activities) {
-			if (a1 == null) {
-				a1 = a2;
+		visitedElements.add(fe);
+
+		if (fe instanceof SubProcess) {
+			SubProcess ssub = (SubProcess) fe;
+			if (ssub.getName() == null || ssub.getName().length() < 4) {
+				num++;
+				elementsBPMN.add(fe);
+				String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l);
+				setElements(fe.getId(), IDProcess, name);
+				temp.append("* name=" + name + " ID=" + fe.getId() + "\n");
+				searchSubProcess(ssub);
 			} else {
-				if (a1.getName().equals(a2.getName())) {
-					flag = false;
-					elementsBPMN.add(a2);
-					String name = a2.getName() != null ? a2.getName() : Messages.getString("Generic.LabelEmpty", l); //$NON-NLS-1$
-					setElements(a2.getId(), IDProcess, name);
-					temp.append("* name=" + name + " ID=" + a2.getId() //$NON-NLS-1$ //$NON-NLS-2$
-							+ "\n"); //$NON-NLS-1$
-					a1 = a2;
-				} else {
-					a1 = a2;
-				}
+				activities.add(ssub);
+				searchSubProcess(ssub);
+			}
+
+		} else if (fe instanceof CallActivity) {
+			CallActivity ca = (CallActivity) fe;
+			if (ca.getName() == null || ca.getName().length() < 4) {
+				num++;
+				elementsBPMN.add(fe);
+				String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l);
+				setElements(fe.getId(), IDProcess, name);
+				temp.append("* name=" + name + " ID=" + fe.getId() + "\n");
+			}
+		} else if (fe instanceof Activity) {
+			Activity a = (Activity) fe;
+			if (a.getName() == null || a.getName().length() < 4) {
+				num++;
+				elementsBPMN.add(fe);
+				String name = fe.getName() != null ? fe.getName() : Messages.getString("Generic.LabelEmpty", l);
+				setElements(fe.getId(), IDProcess, name);
+				temp.append("* name=" + name + " ID=" + fe.getId() + "\n");
+
+			} else {
+				activities.add(a);
 			}
 		}
 
-		if (num > 0 || flag == false) {
-			this.Suggestion += Messages.getString("LabelingActivities.SuggestionSubprocessKO", l) + sub.getName(); //$NON-NLS-1$
-			this.status = false;
-		}
+		visitedElements.remove(fe);
 	}
+
 }
